98f7b8b297c76800e5bb42b59abfd7c7220299d8
aver803bath5 <aver803bath261@gmail.com>
2016-12-19 03:56:10 +0800

    insert user and login and mongodb

--- server.js
+++ server.js
@@ -9,6 +9,60 @@ var request = require('request');
 //var cors = require('cors');
 
 //var logFile = fs.createWriteStream('/var/log/express/access.log');
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+
+// Connection URL
+var url = 'mongodb://139.59.224.238:27017/test';
+
+// Use connect method to connect to the server
+
+function insertUser(email, password) {
+
+	MongoClient.connect(url, function(err, db) {
+
+		assert.equal(null, err);
+		console.log("Connected successfully to server");
+		var collection = db.collection('restaurants');
+		// Find some documents
+		collection.insertMany([
+			{'email' : email, 'password' : password}
+		], function(err, result) {
+			assert.equal(err, null);
+			assert.equal(1, result.result.n);
+			assert.equal(1, result.ops.length);
+			console.log("Inserted 1 documents into the collection");
+		});
+
+	});
+}
+
+function loginCheck(email, password, callback) {
+	MongoClient.connect(url, function(err, db) {
+		var collection = db.collection('restaurants');
+		collection.find({'email': email}).toArray(function(err, docs) {
+			if (password === docs[0].password) {
+				callback(true);
+			}else{
+				callback(false);
+			}
+		}); 
+	});
+}
+
+function checkRepeatEmailByEmail(email, callback){
+	MongoClient.connect(url, function(err, db) {
+		var collection = db.collection('restaurants');
+		collection.find({'email': email}).toArray(function(err, docs) {
+			if (docs.length == 1) {
+				callback(true);
+			}else{
+				callback(false);
+			}
+		}); 
+	});
+}
+
 
 app
 .set('view engine', 'pug')
@@ -26,8 +80,46 @@ app
 	res.render('login');
 })
 
+.post('/checkLogin', (req, res) => {
+	var email = req.body.email;
+	var password = req.body.password;
+	loginCheck(email, password, function(result){
+		if( result == true) {
+			res.redirect('/dashboard');
+			res.end();
+		}else{
+			res.redirect('/login');
+			res.end();
+		}
+	});
+	
+})
+
 .get('/register', (req, res) => {
 	res.render('register');
+})
+
+.post('/insertUser', (req, res) => {
+	var email = req.body.email;
+	var password = req.body.password;
+	var passwordCheck = req.body.passwordCheck;
+    if (password !== passwordCheck) {
+    	res.redirect('/register');
+    	res.end();
+    }
+
+    checkRepeatEmailByEmail(email, function(result) {
+    	if (result == true) {
+    		res.redirect(302, '/register');
+    		res.end();
+    	}else{
+    		insertUser(email, password);
+    		res.redirect('/login');
+    		res.end();
+    	}
+    });
+
+    
 })
 
 .get('/dashboard', (req, res) => {
@@ -41,7 +133,7 @@ app
 		'&scope=public_profile'+
 		'&response_type=code';
 	res.redirect(facebook_oauth_url);
-	//res.send(JSON.stringify({'redirect_url':facebook_oauth_url}));
+	//return res.send(JSON.stringify({'redirect_url':facebook_oauth_url}));
 })
 
 .get('/auth/facebook/callback', (req, res) => {
@@ -64,14 +156,14 @@ var code = req.query.code;
 		};
 		request(info_option, (err, response, body) => {
 			if(err){
-				res.send(err);
+				return res.send(err);
 			}
 
 			request({url:'https://graph.facebook.com/me?access_token=' + access_token}, (err, response, body) => {
 				if(err){
-					res.send(err);
+					return res.send(err);
 				}else{
-					res.send(body);
+					return res.send(body);
 				}
 			});
 		});
