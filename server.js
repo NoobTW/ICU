d749c838eafb2cc9fa17ba3e10571ddc8ede2580
NoobTW <noob@noob.tw>
2016-12-19 20:15:27 +0800

    add session, fix RESTful name

--- server.js
+++ server.js
@@ -1,151 +1,173 @@
+var fs = require('fs');
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
 var pug = require('pug');
-var port = process.env.PORT || 10150;
-var fs = require('fs');
 var request = require('request');
-//var logger = require('morgan');
-//var cors = require('cors');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var config = require('./config');
 
-//var logFile = fs.createWriteStream('/var/log/express/access.log');
-var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
+var mongo = require('mongodb');
+var mc = mongo.MongoClient;
+var app = express();
 
-// Connection URL
-var url = 'mongodb://139.59.224.238:27017/test';
-
-// Use connect method to connect to the server
-
-function insertUser(email, password) {
-
-	MongoClient.connect(url, function(err, db) {
-
-		assert.equal(null, err);
-		console.log("Connected successfully to server");
-		var collection = db.collection('restaurants');
-		// Find some documents
-		collection.insertMany([
-			{'email' : email, 'password' : password}
-		], function(err, result) {
-			assert.equal(err, null);
-			assert.equal(1, result.result.n);
-			assert.equal(1, result.ops.length);
-			console.log("Inserted 1 documents into the collection");
-		});
-
-	});
-}
-
-function loginCheck(email, password, callback) {
-	MongoClient.connect(url, function(err, db) {
-		var collection = db.collection('restaurants');
-		collection.find({'email': email}).toArray(function(err, docs) {
-			if(!err && docs.length){
-				if(password === docs[0].password){
-					callback(true);
-				}else{
-					callback(false);
-				}
-			}else{
-				callback(false);
-			}
-		}); 
-	});
-}
-
-function checkRepeatEmailByEmail(email, callback){
-	MongoClient.connect(url, function(err, db) {
-		var collection = db.collection('restaurants');
-		collection.find({'email': email}).toArray(function(err, docs) {
-			if (docs.length == 1) {
-				callback(true);
-			}else{
-				callback(false);
-			}
-		}); 
-	});
-}
-
+const HOST_MONGO = 'mongodb://139.59.224.238:27017/test';
+const port = process.env.PORT || 10150;
+const FB_APP_ID = '1800155283585541';
+const FB_APP_KEY = '5f559de2468c506036c10164a0c8adff';
+const MIME_JSON = {'Content-Type': 'application/javascript'}
 
 app
 .set('view engine', 'pug')
 .use(bodyParser.urlencoded({extended: true}))
 .use(bodyParser.json())
-//.use(cors())
-//.use(logger({stream: logFile}))
-
-app.listen(port, () => {
-	console.log("SERVER STARTED");
+.use(session({
+	secret: config.secret.session,
+	cookie: { maxAge: 10 * 60 * 1000 },
+	resave: false,
+	saveUninitialized: true
+}))
+.use(cookieParser(config.secret.cookie))
+.listen(port, () => {
+	console.log('SERVER STARTED');
 });
 
+
 app
+.get('/', (req, res) => {
+	var sess = req.session;
+	if(sess.email){
+		console.log(sess.email);
+		res.render('dashboard', {
+			email: sess.email
+		});
+	}else{
+		res.redirect('/login');
+	}
+})
+
 .get('/login', (req, res) => {
 	res.render('login');
 })
 
-.post('/checkLogin', (req, res) => {
-	var email = req.body.email;
-	var password = req.body.password;
-	loginCheck(email, password, function(result){
-		if( result == true) {
-			res.redirect('/dashboard');
-			res.end();
-		}else{
-			res.redirect('/login');
-			res.end();
-		}
-	});
-	
+.post('/login', (req, res) => {
+	var data = req.body;
+	var sess = req.session;
+
+	if(!data.email || !data.password){
+		var result = {
+			result: -1
+		};
+		res.writeHead(401, MIME_JSON);
+		res.write(JSON.stringify(result));
+		res.end();
+	}else{
+		mc.connect(HOST_MONGO, (err, db) => {
+			var collection = db.collection('user');
+			collection.find({'email': data.email}).toArray((err, docs) => {
+				if(!err && docs.length){
+					if(docs[0].password === data.password){
+						sess.email = docs[0].email;
+						var result = {
+							result: 0
+						};
+						res.writeHead(200, MIME_JSON);
+						res.write(JSON.stringify(result));
+						res.end();
+					}else{
+						var result = {
+							result: -1
+						};
+						res.writeHead(401, MIME_JSON);
+						res.write(JSON.stringify(result));
+						res.end();
+					}
+				}else if(!err){
+					var result = {
+						result: -1
+					};
+					res.writeHead(401, MIME_JSON);
+					res.write(JSON.stringify(result));
+					res.end();
+				}else{
+					var result = {
+						result: -999
+					};
+					console.log(err);
+					res.writeHead(500, MIME_JSON);
+					res.write(JSON.stringify(result));
+					res.end();
+				}
+			});
+		});
+	}
 })
 
 .get('/register', (req, res) => {
 	res.render('register');
 })
 
-.post('/insertUser', (req, res) => {
-	var email = req.body.email;
-	var password = req.body.password;
-	var passwordCheck = req.body.passwordCheck;
-    if (password !== passwordCheck) {
-    	res.redirect('/register');
-    	res.end();
-    }
+.post('/register', (req, res) => {
+	var data = req.body;
 
-    checkRepeatEmailByEmail(email, function(result) {
-    	if (result == true) {
-    		res.redirect(302, '/register');
-    		res.end();
-    	}else{
-    		insertUser(email, password);
-    		res.redirect('/login');
-    		res.end();
-    	}
-    });
-
-    
-})
-
-.get('/dashboard', (req, res) => {
-	res.render('dashboard');
+	mc.connect(HOST_MONGO, (err, db) => {
+		var collection = db.collection('user');
+		collection.find({'email': data.eamil}).toArray((err, docs) => {
+			if(!err && docs.length){
+				var result = {
+					result: -2
+				};
+				res.writeHead(400, MIME_JSON);
+				res.write(JSON.stringify(result));
+				res.end();
+			}else if(!err){
+				collection.insert({
+					email: data.email,
+					password: data.password
+				}, (err, resp) => {
+					if(!err){
+						var result = {
+							result: 0
+						};
+						res.writeHead(200, MIME_JSON);
+						res.write(JSON.stringify(result));
+						res.end();
+					}else{
+						var result = {
+							result: -999
+						};
+						res.writeHead(500, MIME_JSON);
+						res.write(JSON.stringify(result));
+						res.end();
+					}
+				})
+			}else{
+				var result = {
+					result: -999
+				};
+				res.writeHead(500, MIME_JSON);
+				res.write(JSON.stringify(result));
+				res.end();
+			}
+		})
+	});
 })
 
 .get('/auth/facebook', (req, res) => {
 	var facebook_oauth_url = 'https://www.facebook.com/dialog/oauth?' +
 		'redirect_uri=http://localhost:10150/auth/facebook/callback'+
-		'&client_id=1800155283585541' +
-		'&scope=public_profile'+
+		'&client_id=' + FB_APP_ID +
+		'&scope=public_profile,email'+
 		'&response_type=code';
 	res.redirect(facebook_oauth_url);
-	//return res.send(JSON.stringify({'redirect_url':facebook_oauth_url}));
 })
 
 .get('/auth/facebook/callback', (req, res) => {
 var code = req.query.code;
 	var token_option = {
-		url:'https://graph.facebook.com/v2.3/oauth/access_token?' +
-			'client_id=1800155283585541' +
-			'&client_secret=5f559de2468c506036c10164a0c8adff' +
+		url:'https://graph.facebook.com/v2.8/oauth/access_token?' +
+			'client_id=' + FB_APP_ID +
+			'&client_secret=' + FB_APP_KEY +
 			'&code=' + code +
 			'&redirect_uri=' + 'http://localhost:10150/auth/facebook/callback',
 		method:"GET"
@@ -155,15 +177,14 @@ var code = req.query.code;
 		var info_option = {
 		url:'https://graph.facebook.com/debug_token?'+
 		'input_token='+access_token +
-		'&access_token=1800155283585541',
+		'&access_token=' + FB_APP_ID,
 		method:"GET"
 		};
 		request(info_option, (err, response, body) => {
 			if(err){
 				return res.send(err);
 			}
-
-			request({url:'https://graph.facebook.com/me?access_token=' + access_token}, (err, response, body) => {
+			request({url:'https://graph.facebook.com/me?fields=name,email&access_token=' + access_token}, (err, response, body) => {
 				if(err){
 					return res.send(err);
 				}else{
